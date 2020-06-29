b3bd4fb6dea9c4ce7e47320505fcd37c1b8ce410
ajibolashodipo <shodipoajibola@gmail.com>
2020-06-29 19:59:49 +0100

    added winning algorithm for x and o. draw algo next

--- src/Board.js
+++ src/Board.js
@@ -3,6 +3,7 @@ import "./Board.css"
 
 class Board extends Component {
   state = {
+    zero: { value: null, clicked: false },
     one: { value: null, clicked: false },
     two: { value: null, clicked: false },
     three: { value: null, clicked: false },
@@ -11,8 +12,19 @@ class Board extends Component {
     six: { value: null, clicked: false },
     seven: { value: null, clicked: false },
     eight: { value: null, clicked: false },
-    nine: { value: null, clicked: false },
-    clickCount: 0
+    clickCount: 0,
+    playerOne: [],
+    playerTwo: [],
+    waysToWin: [
+      ["zero", "one", "two"],
+      ["three", "four", "five"],
+      ["six", "seven", "eight"],
+      ["zero", "three", "six"],
+      ["one", "four", "seven"],
+      ["two", "five", "eight"],
+      ["zero", "four", "eight"],
+      ["two", "four", "six"]
+    ]
   }
 
   handleClick = (e) => {
@@ -32,11 +44,77 @@ class Board extends Component {
       },
       (e) => {
         // console.log(clickCount)
-        console.log(clickCount)
-        console.log(this.state)
-        console.log(deck)
+        // console.log(this.state)
+
+        // map player one to array
+        let jb = mapPlayertoArray(this.state.playerOne)
+        //check if player one wins and store its return value
+        let returnPlayerOne = checkWinnerPlayerOne(this.state.waysToWin, jb)
+        // map player two to array
+        let show = mapPlayertoArray(this.state.playerTwo)
+        //check if player two wins and store its return value
+        let returnPlayerTwo = checkWinnerPlayerTwo(this.state.waysToWin, show)
+
+        //
+        //check for ties
+        // console.log(returnPlayerTwo)
+        checkTie(returnPlayerOne, returnPlayerTwo)
+        console.log(checkWinnerPlayerOne(this.state.waysToWin, jb))
       }
     )
+
+    //use counter to separate them
+    if (clickCount % 2 === 0) {
+      let playerData = { gridID: e.target.id, gridChar: deck[clickCount] }
+      const playerOne = [...this.state.playerOne, playerData]
+      this.setState({ playerOne: playerOne })
+    } else {
+      let playerData = { gridID: e.target.id, gridChar: deck[clickCount] }
+      const playerTwo = [...this.state.playerTwo, playerData]
+      this.setState({ playerTwo: playerTwo })
+    }
+
+    //player one winning algo
+    function mapPlayertoArray(player) {
+      //map out to another array the grid element names
+      const plOneGridEl = player.map((el) => {
+        return el.gridID
+      })
+      return plOneGridEl
+    }
+
+    function checkWinnerPlayerOne(waysToWin, plOneGridEl) {
+      waysToWin.forEach((perm) => {
+        if (
+          plOneGridEl.includes(perm[0]) &&
+          plOneGridEl.includes(perm[1]) &&
+          plOneGridEl.includes(perm[2])
+        ) {
+          console.log("x wins")
+          return true
+        }
+        return false
+      })
+    }
+    function checkWinnerPlayerTwo(waysToWin, plOneGridEl) {
+      waysToWin.forEach((perm) => {
+        if (
+          plOneGridEl.includes(perm[0]) &&
+          plOneGridEl.includes(perm[1]) &&
+          plOneGridEl.includes(perm[2])
+        ) {
+          console.log("o wins")
+          return true
+        }
+        return false
+      })
+    }
+    function checkTie(plOne, plTwo) {
+      // console.log(waysToWin)
+      if (plOne === false && plTwo === false) {
+        console.log("draw")
+      }
+    }
   }
   render() {
     // destructuring
@@ -46,6 +124,9 @@ class Board extends Component {
     return (
       <div>
         <div className="grid-container">
+          <div id="zero" className="item item-0" onClick={this.handleClick}>
+            {this.state.zero.value}
+          </div>
           <div id="one" className="item item-1" onClick={this.handleClick}>
             {this.state.one.value}
           </div>
@@ -69,9 +150,6 @@ class Board extends Component {
           </div>
           <div id="eight" className="item item-8" onClick={this.handleClick}>
             {this.state.eight.value}
-          </div>
-          <div id="nine" className="item item-9" onClick={this.handleClick}>
-            {this.state.nine.value}
           </div>
         </div>
       </div>
