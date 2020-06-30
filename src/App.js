4bddba7ed7b244e2373bd8a88cb6aaded08da89a
ajibolashodipo <shodipoajibola@gmail.com>
2020-06-30 17:21:23 +0100

    added app level state and made progress with its implementation

--- src/App.js
+++ src/App.js
@@ -4,13 +4,52 @@ import PlayerSelect from "./PlayerSelect"
 import Board from "./Board"
 import ScoreBoard from "./ScoreBoard"
 
-export class App extends Component {
+class App extends Component {
+  state = {
+    playerOneChar: "",
+    playerTwoChar: "",
+    deck: ["x", "o", "x", "o", "x", "o", "x", "o", "x"]
+  }
+
+  registerPlayer = (data) => {
+    const extractKey = Object.keys(data)
+    console.log(data[extractKey[1]])
+    this.setState(
+      {
+        ...this.state,
+        [extractKey[0]]: data[extractKey[0]],
+        [extractKey[1]]: data[extractKey[1]]
+      },
+      () => {
+        console.log(this.state)
+      }
+    )
+  }
+
+  loadDeck = (char1, char2) => {
+    const myArray = []
+    for (let i = 0; i < 9; i++) {
+      if (!(i % 2)) {
+        myArray.push(char1)
+      } else {
+        myArray.push(char2)
+      }
+    }
+    this.setState({ ...this.state, deck: myArray }, () => {
+      return myArray
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>reacTic Tac Toe</h1>
-        <PlayerSelect />
-        <Board deck={["x", "o", "x", "o", "x", "o", "x", "o", "x"]} />
+        <PlayerSelect
+          registerPlayer={this.registerPlayer}
+          loadDeck={this.loadDeck}
+        />
+
+        <Board deck={this.state.deck} />
         <ScoreBoard />
       </div>
     )
--- src/PlayerSelect.js
+++ src/PlayerSelect.js
@@ -1,15 +1,45 @@
 import React, { Component } from "react"
 
 class PlayerSelect extends Component {
+  state = {
+    playerOneChar: "",
+    playerTwoChar: ""
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.registerPlayer(this.state)
+    this.props.loadDeck(this.state.playerOneChar, this.state.playerTwoChar)
+    this.setState({
+      playerOneChar: "",
+      playerTwoChar: ""
+    })
+  }
   render() {
     return (
       <div>
         <p>Choose your character</p>
-        <form action="">
-          <label htmlFor="player1">Player 1:</label>
-          <input type="text" id="player1" />
-          <label htmlFor="player2">Player 2:</label>
-          <input type="text" id="player2" />
+        <form action="" onSubmit={this.handleSubmit}>
+          <label htmlFor="playerOneChar">Player 1:</label>
+          <input
+            type="text"
+            id="playerOneChar"
+            onChange={this.handleChange}
+            value={this.state.playerOneChar}
+          />
+          <label htmlFor="playerTwoChar">Player 2:</label>
+          <input
+            type="text"
+            id="playerTwoChar"
+            onChange={this.handleChange}
+            value={this.state.playerTwoChar}
+          />
           <button>Let's Play</button>
         </form>
       </div>
